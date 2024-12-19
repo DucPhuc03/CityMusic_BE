15553be93f3353f19721a37063df69c61c012e3b
DucPhuc03 <nguyenpguc2003@gmail.com>
2024-12-19 18:13:10 +0700

    start

---
+++ src/main/java/com/citymusic/CityMusicApplication.java
@@ -0,0 +1,13 @@
+package com.citymusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/citymusic/CityMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citymusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
